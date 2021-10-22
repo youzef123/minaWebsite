e2a46d1ffb972721ddd0dbc386c0ef0a2c6f08db
Yousef Tayuan <biboytayuan@gmail.com>
2021-10-22 16:05:43 +0800

    first commit

---
+++ Dom.js
@@ -0,0 +1,81 @@
+const hover = document.querySelector('.overlay')
+const penguinDetect = document.querySelector('.container1')
+const minaTransition = document.querySelector('.minaTrans')
+let destination = document.getElementById("typedtext")
+hover.addEventListener("mouseover",  typewriter)
+penguinDetect.addEventListener("mouseover",  () =>{
+  minaTransition.style.visibility = 'hidden'
+});
+
+
+ outMouse = () =>{
+  minaTransition.style.visibility = 'visible'
+};
+
+
+
+// our element
+
+
+
+function clear(){
+
+}
+var aText = new Array(
+    "“Keep working hard, do the best that you can, and everything will fall into place." , 
+   "<h4>-Mina</h4>"
+    );
+    var iSpeed = 100; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+  
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+  
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+      if ( iIndex === aText.length ) {
+        
+       }
+     } else {
+
+      setTimeout("typewriter()", iSpeed);
+     }
+
+
+
+
+
+    }
+  
+    
+  
+    
+    // if(User.value.length === 0 || 
+    //     Psw.value.length ===0){
+    //         msg.innerHTML = errorText;
+    //      console.log("ERror")   
+    // }else{
+    //     if(User.value == "Minacute123" && Psw.value == "sharon123"){
+    //         window.alert("Success Penguin");
+    //     }
+    // }
+
+
