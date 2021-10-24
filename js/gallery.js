e22a41b99bc12e4b6ae143da24ff116f1c35ce0b
Yousef Tayuan <biboytayuan@gmail.com>
2021-10-24 13:58:30 +0800

    version1

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+//container
+const container = document.querySelector(".container");
+const innerChild = container.children;
+const text = document.querySelector(".quote");
+
+text.innerHTML = `<h6>Rather than being paralysed by worry,if we continue doing little by little the things that are infront of us like this,I think we will gradually, slowly, be able to move forwards.<br>   <b>-Mina</b></h6>`;
+
+
+console.log(innerChild)
