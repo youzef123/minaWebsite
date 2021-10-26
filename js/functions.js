a1b81da7ec4583d1a79deb76eb8a4e33bf16728d
Yousef Tayuan <biboytayuan@gmail.com>
2021-10-26 18:11:30 +0800

    itsResponsive

--- js/functions.js
+++ js/functions.js
@@ -2,33 +2,34 @@ const hover = document.querySelector('.overlay')
 const penguinDetect = document.querySelector('.container1')
 const minaTransition = document.querySelector('.minaTrans')
 let destination = document.getElementById("typedtext")
-hover.addEventListener("mouseover",  typewriter)
 penguinDetect.addEventListener("mouseover",  () =>{
   minaTransition.style.visibility = 'hidden'
 });
- outMouse = () =>{
+
+
+outMouse = () =>{
   minaTransition.style.visibility = 'visible'
 };
+// destination.addEventListener("mouseover", type)
+const quote = ["Keep working hard, do the best that you can, and everything will fall into place. -Mina"]
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
 
-// const quote = ["Keep working hard, do the best that you can, and everything will fall into place. <h4>-Mina</h4>"]
-// let count = 0;
-// let index = 0;
-// let currentText = "";
-// let letter = "";
-
-// (function type(){
-//   if(count === quote.length){
-//     count = 0;
-//   }
-//   currentText = quote[count];
-//   letter = currentText.splice(0 , ++index);
-//   destination.textContent = letter;
-//   if(letter === currentText.length){
-//     count++;
-//     index = 0 ;
-//   }
-//   setTimeout(type, 400);
-// }());
+(function type(){
+  if(count === quote.length){
+    count = 0;
+  }
+  currentText = quote[count];
+  letter = currentText.slice(0 , ++index);
+  destination.textContent = letter;
+  if(letter === currentText.length){
+    count++;
+    index = 0 ;
+  }
+  setTimeout(type, 50);
+}());
 
 
 // our element
@@ -36,45 +37,10 @@ penguinDetect.addEventListener("mouseover",  () =>{
 
 
 
-var aText = new Array(
-    "“Keep working hard, do the best that you can, and everything will fall into place." , 
-   "<h4>-Mina</h4>"
-    );
-    var iSpeed = 100; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
-     
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
-  
-    function typewriter()
-    {
-     sContents =  ' ';
-     iRow = Math.max(0, iIndex-iScrollAt);
-  
-     
-     while ( iRow < iIndex ) {
-      sContents += aText[iRow++] + '<br />';
-     }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 500);
-      }
-      if ( iIndex === aText.length ) {
-        
-       }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
-
-    }
-  
+// let aText = [
+//     "“Keep working hard, do the best that you can, and everything will fall into place." , 
+//    "<h4>-Mina</h4>"]
+ 
     
   
     
